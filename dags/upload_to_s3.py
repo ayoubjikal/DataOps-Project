ad952bfc2591ee1bfcdf943856a591740b8d778e
ayoubjikal <ayoubjikal0@gmail.com>
2025-11-25 20:43:43 +0100

    Revert "Import project to github"
    This reverts commit 3c6dcd9bcb95b875fe75ef7adf89e95e864c1b88.

--- dags/upload_to_s3.py
+++ dags/upload_to_s3.py
@@ -1,44 +1,23 @@
-# dags/load_s3_to_snowflake.py
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.hooks.base import BaseHook
-from airflow.operators.python import PythonOperator
-import snowflake.connector
+# from airflow.utils.dates import days_ago
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 
-default_args = {
-    "owner": "ayoub",
-    "depends_on_past": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5)
-}
-def creat_warehouse():
-    return " CREATE WAREHOUSE IF NOT EXISTS ecommerce_wh WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60 AUTO_RESUME = TRUE INITIALLY_SUSPENDED = TRUE;"
-
-def upload_snowflake():
-    conn = BaseHook.get_connection("snowflake_conn")
-    sf = snowflake.connector.connect(
-        user=conn.login,
-        password=conn.password,
-        account=conn.extra_dejson.get("account"),
-        warehouse=conn.extra_dejson.get("warehouse"),
-        database=conn.extra_dejson.get("database"),
-        schema=conn.schema,
-        role=conn.extra_dejson.get("role")
-    )
-    cur = sf.cursor()
-    cur.execute("  COPY INTO ecommerce_table FROM @ecommerce_stage FILE_FORMAT = my_csv_format")
-    print(cur.fetchone())
-    sf.close()
 
 with DAG(
-    dag_id="load_s3_to_snowflake",
-    start_date=datetime(2025, 1, 1),
-    schedule="@daily",
-    default_args=default_args,
-    catchup=False,
-    tags=["load", "snowflake", "s3"],
-) as dag:
+    dag_id='upload_raw_to_s3',
+    # schedule_interval=None,
+    # start_date = days_ago(1)
 
-    t1 = PythonOperator(task_id='test_snowflake', python_callable=upload_snowflake)
-
+    ) as dag:
+    
+    upload_to_s3 = LocalFilesystemToS3Operator(
+        task_id='upload_to_s3',
+        filename="/usr/local/airflow/include/data/data.csv",
+        dest_key="raw/",
+        dest_bucket="ecommerce-dataops",
+        aws_conn_id='aws_default', 
+        
+    )
+    
+    
+    upload_to_s3
